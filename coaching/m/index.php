2c174b3463a375bf1a9d20d8bc588b42a57c7372
Дмитрий Китаев <idems.91@gmail.com>
2018-10-16 03:41:55 +0700

    123

--- coaching/m/index.php
+++ coaching/m/index.php
@@ -1008,21 +1008,21 @@ require_once("prices.php");
 		</div> -->
 		<div class="b-block">
 			<div class="b-new-record b-new-review-block">
-				<h5 class="b-small"><b>Такой метод</b> внедрения маркетинговых<br> инструментов <b>позволяет расти<br>без вложений</b>
+				<h5 class="b-small"><b>6 из 10 специалистов</b> окупают нашу<br> программу ещё во время прохождения</h5>
 				<p class="b-new-small"></p>
-				<h5 class="b-new-big"><b>212 000 рублей</b> в первую неделю<br>прохождения курса</h5>
+				<h5 class="b-new-big"><b>Получили 180 заявок по 50 рублей.<br></b>Курс окупился за первый же день</h5>
 				<div class="b-new-record-inner b-new-slide clearfix">
 					<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-						<iframe src="https://www.youtube.com/embed/ggLvqCFvVNg?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						<iframe src="https://www.youtube.com/embed/clb2cY9q5Og?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 						<a href='#' class="b-play"></a>
 					</div>
 					<img src="i/reviews/review-1.jpg">
 				</div>
 				<p class="b-new-small"></p>
-				<h5 class="b-new-big">Никогда не было таких результатов.<br><b>Сразу 11 заявок по 120 рублей</b> на кухни</h5>
+				<h5 class="b-new-big">Продал сайт турецкой компании за 200 000 рублей.<br><b>Средний чек вырос в 4 раза</b></h5>
 				<div class="b-new-record-inner b-new-slide clearfix">
 					<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-						<iframe src="https://www.youtube.com/embed/809tKyXN6Q0?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						<iframe src="https://www.youtube.com/embed/JxoXHB9wSTE?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 						<a href='#' class="b-play"></a>
 					</div>
 					<img src="i/reviews/review-2.jpg">
@@ -1031,7 +1031,7 @@ require_once("prices.php");
 				<h5 class="b-new-big"><b>212 000 рублей</b> в первую неделю<br>прохождения курса</h5>
 				<div class="b-new-record-inner b-new-slide clearfix">
 					<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-						<iframe src="https://www.youtube.com/embed/U28mfY0raq0?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						<iframe src="https://www.youtube.com/embed/ggLvqCFvVNg?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 						<a href='#' class="b-play"></a>
 					</div>
 					<img src="i/reviews/review-3.jpg">
@@ -1041,7 +1041,7 @@ require_once("prices.php");
 					<h5 class="b-new-big"><b>Чек-листы это палочка-выручалочка.</b><br>Сократили стоимость заявки до 60 рублей.</h5>
 					<div class="b-new-record-inner b-new-slide clearfix">
 						<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-							<iframe src="https://www.youtube.com/embed/Ac_iMXo11D8?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+							<iframe src="https://www.youtube.com/embed/5MbvIlXVnNk?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 							<a href='#' class="b-play"></a>
 						</div>
 						<img src="i/reviews/review-4.jpg">
@@ -1050,7 +1050,7 @@ require_once("prices.php");
 					<h5 class="b-new-big">Запустили 3 сайта, цена лида от 79 рублей.<br><b>За 1 неделю окупил курс</b></h5>
 					<div class="b-new-record-inner b-new-slide clearfix">
 						<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-							<iframe src="https://www.youtube.com/embed/KrSdOyjGMjY?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+							<iframe src="https://www.youtube.com/embed/NOlNJYAuHvE?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 							<a href='#' class="b-play"></a>
 						</div>
 						<img src="i/reviews/review-5.jpg">
@@ -1059,12 +1059,11 @@ require_once("prices.php");
 					<h5 class="b-new-big">Никогда не было таких результатов.<br><b>Сразу 11 заявок по 120 рублей</b> на кухни</h5>
 					<div class="b-new-record-inner b-new-slide clearfix">
 						<div class="b-video-block b-video-review" style="background-image: url('i/2/video.jpg');">
-							<iframe src="https://www.youtube.com/embed/ZKRX6vaJmq4?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+							<iframe src="https://www.youtube.com/embed/809tKyXN6Q0?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 							<a href='#' class="b-play"></a>
 						</div>
 						<img src="i/reviews/review-6.jpg">
 					</div>
-				<img class="review-image" src="i/reviews/pic.jpg">
 				</div>
 				<div class="b-note-btn">
 					<a href="#" class="b-btn b-btn-orange b-btn-form" style="padding: 30px 42px 34px 42px;">
